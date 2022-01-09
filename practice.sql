347eb31323014e090192b34ebd8eebfc6f89227e
looSeeCan <95648904+looSeeCan@users.noreply.github.com>
2022-01-09 01:37:14 -0600

    Initial commit

---
+++ practice.sql
@@ -0,0 +1,6 @@
+-- The select statement. SELECT, FROM, WHERE, ORDER BY; are all clases. They should be on thier own lines for clarity. (but dont have to be)
+--  
+SELECT *
+FROM customers  
+WHERE customer_id = 3
+ORDER BY first_name
---
+++ theSelectStatement.sql
@@ -0,0 +1,7 @@
+USE sql_store; -- selects the sql_store database 
+-- The select statement. SELECT, FROM, WHERE, ORDER BY; are all clases. They should be on thier own lines for clarity. (but dont have to be)
+--  
+SELECT *
+FROM customers -- these clauses here have to be used in order
+WHERE customer_id = 3 -- i can omit this clause though. I do not think i can use it after ORDER BY 
+ORDER BY first_name
